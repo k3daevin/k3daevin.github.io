7a0c8ba4c5a5cd7cbb2e4bb16b3bb02b577137b6
k3daevin <kevindrzyc@gmail.com>
2021-08-14 09:12:24 +0200

    valid als func

--- fotzbeck/intro.js
+++ fotzbeck/intro.js
@@ -1,17 +1,19 @@
 let Intro = class extends Screen {
     constructor() {
       super();
-      this.valid = X > Y
-      if (this.valid == false) {
+      if (this.valid() == false) {
         startButton.hide()
         highscoreButton.hide()
       }
       this.highscore = highscore.read()
     }
+    valid() {
+      return X > Y
+    }
     draw(x, y) {
       background(255)
       textSize(32);
-      if (this.valid) {
+      if (this.valid()) {
         text('Fotzbeck v1.2.1 geht glei los! Klick für Start!', 10, 30);
       } else {
         text('Fotzbeck v1.2.1 wird QUER gespielt du Fotz! Klick für Neustart!', 10, 30);
@@ -20,11 +22,6 @@ let Intro = class extends Screen {
       text(`Highscore: ${this.highscore}`, 10, 70)
     }
     click(x, y) {
-      return
-      if (this.valid) {
-        currentScreen = new Game()
-      } else {
-        location.reload()
-      }
+
     }
   }
